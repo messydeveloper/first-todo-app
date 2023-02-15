cfa53213065c7b89dd7639abe4f6512f9449a211
Enard <enard@Enards-MacBook-Pro.local>
2023-02-15 14:43:35 +1100

    export delete, and update todo

--- src/todo/queries.js
+++ src/todo/queries.js
@@ -1,13 +1,15 @@
-const getTodos ="SELECT * FROM todo";
-const getTodoById = "SELECT * FROM todo WHERE id = $1";
-const checkTodoIfExists = "SELECT t FROM todo t WHERE t.name = $1"
-const createTodo = "INSERT INTO todo (name, done, language) VALUES($1, $2, $3)";
-const deleteTodo = "DELETE FROM todo WHERE id = $1";
+const getTodos ="SELECT * FROM todos";
+const getTodoById = "SELECT * FROM todos WHERE id = $1";
+const checkTodoIfExists = "SELECT t FROM todos t WHERE t.name = $1"
+const createTodo = "INSERT INTO todos (name, done, language) VALUES($1, $2, $3)";
+const deleteTodo = "DELETE FROM todos WHERE id = $1";
+const updateTodo = "UPDATE todos SET name=$1, done=$2, language=$3 WHERE id = $4";
 
 module.exports = {
     getTodos,
     getTodoById,
     checkTodoIfExists,
     createTodo,
-    deleteTodo
+    deleteTodo,
+    updateTodo
 }
