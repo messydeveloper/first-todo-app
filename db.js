9c4055dcc0389ce58e75b2d8a21f861d2452224c
Enard <enard@Enards-MBP.modem>
2023-02-19 11:16:15 +1100

    db

--- db.js
+++ db.js
@@ -1,9 +1,9 @@
 const pg = require('pg');
 const pool = new pg.Pool({
-    user:"postgres",
-    host:"localhost",
-    database:"todoapp",
-    password:"123",
+    user:"todoapp_9a9i",
+    host:"dpg-cfomioqrrk0fd9pr7cag-a",
+    database:"todoapp_9a9i_user",
+    password:"jkXmEnPRSOdkFcOc7v3epksRVV5jKCsP",
     port:5432
 });
 
