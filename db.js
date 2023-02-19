e14c6bf96260ace87957068b6efae17b5157a9bb
Enard <enard@Enards-MBP.modem>
2023-02-19 19:03:02 +1100

    add heroku db config

--- db.js
+++ db.js
@@ -1,10 +1,14 @@
 const pg = require('pg');
 const pool = new pg.Pool({
-    user: process.env.DB_USER,
-    host:process.env.DB_HOST,
-    database:process.env.DB,
-    password:process.env.DB_PW,
-    port:process.env.DB_PORT
+    // user: process.env.DB_USER,
+    // host:process.env.DB_HOST,
+    // database:process.env.DB,
+    // password:process.env.DB_PW,
+    // port:process.env.DB_PORT
+    connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = pool;
