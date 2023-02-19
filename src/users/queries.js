cde0a741a6d5c1eb94e40c27a612584de82b7e36
Enard <enard@Enards-MBP.modem>
2023-02-20 09:12:02 +1100

    fix query statements

--- src/users/queries.js
+++ src/users/queries.js
@@ -1,5 +1,5 @@
-const getUserSession = "SELECT * FROM session WHERE sid = $1";
-const deleteSession = "DELETE FROM session WHERE sid = $1";
+const getUserSession = "SELECT * FROM session WHERE sid = '$1'";
+const deleteSession = "DELETE FROM session WHERE sid = '$1'";
 const getAllUsers = "SELECT * FROM users";
 const signup = "INSERT INTO users (email, password, role) VALUES ($1, $2, $3)";
 const login = "SELECT * from users WHERE email = $1";
