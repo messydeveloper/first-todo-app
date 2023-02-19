09d09adb5512a9918c23770d1385fa0eb52590b7
Enard <enard@Enards-MBP.modem>
2023-02-19 12:17:26 +1100

    remove port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const userRoutes = require('./src/users/routes');
 
 const express = require('express');
 const app = express();
-const port = 3000;
+// const port = 3000;
 const passport = require('passport');
 const session = require('express-session');
 const bodyParser = require('body-parser')
@@ -44,6 +44,6 @@ app.use(passport.session());
 app.use('/api/v1/todos', todoRoutes);
 app.use('/api/v1/users', userRoutes);
 
-app.listen(port, () => {
-    console.log(`listening on port: ${port}`);
-});
+// app.listen(port, () => {
+//     console.log(`listening on port: ${port}`);
+// });
