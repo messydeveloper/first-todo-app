daf21d0838a288858de3bdfb133163a1803ba49e
Enard <enard@Enards-MBP.modem>
2023-02-20 09:05:32 +1100

    modify db connection

--- db.js
+++ db.js
@@ -1,25 +1,20 @@
-// const pg = require('pg');
-const {Client, Pool} = require('pg');
-const pgSession = require('connect-pg-simple');
+const pg = require('pg');
+const {Client} = require('pg');
 
-// const pool = new Client({
-//     connectionString: process.env.DATABASE_URL,
-//     ssl: {
-//       rejectUnauthorized: false
-//     }
-//   });
+const pool = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    }
+  });
   
-// pool.connect();
-const pool = new Pool({
-    user: process.env.DB_USER,
-    host:process.env.DB_HOST,
-    database:process.env.DB,
-    password:process.env.DB_PW,
-    port:process.env.DB_PORT
-    // user:'postgres',
-    // host:'localhost',
-    // database:'todoapp',
-    // port:5432
-});
+pool.connect();
+// const pool = new pg.Pool({
+//     user: process.env.DB_USER,
+//     host:process.env.DB_HOST,
+//     database:process.env.DB,
+//     password:process.env.DB_PW,
+//     port:process.env.DB_PORT
+// });
 
 module.exports = pool;
