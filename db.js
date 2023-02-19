a91552387274b751a5d3aac005c71d2cd958d29c
Enard <enard@Enards-MBP.modem>
2023-02-19 19:34:51 +1100

    configure db connection to heroku pg

--- db.js
+++ db.js
@@ -1,14 +1,20 @@
 const pg = require('pg');
-const pool = new pg.Pool({
-    // user: process.env.DB_USER,
-    // host:process.env.DB_HOST,
-    // database:process.env.DB,
-    // password:process.env.DB_PW,
-    // port:process.env.DB_PORT
+const {Client} = require('pg');
+
+const pool = new Client({
     connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
-});
+    ssl: {
+      rejectUnauthorized: false
+    }
+  });
+  
+pool.connect();
+// const pool = new pg.Pool({
+//     user: process.env.DB_USER,
+//     host:process.env.DB_HOST,
+//     database:process.env.DB,
+//     password:process.env.DB_PW,
+//     port:process.env.DB_PORT
+// });
 
 module.exports = pool;
