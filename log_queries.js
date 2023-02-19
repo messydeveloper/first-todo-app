39905a9f35d5502ef412b254582ee14deaca33e2
Enard <enard@Enards-MBP.modem>
2023-02-20 08:03:46 +1100

    fix getUserLogs query

--- log_queries.js
+++ log_queries.js
@@ -1,4 +1,4 @@
-const getUserLogs = "SELECT * FROM user_logs WHERE uid = $1 AND role = 'users'";
+const getUserLogs = "SELECT * FROM user_logs WHERE uid = $1 AND role = 'user'";
 const sendLoginDate = "INSERT INTO user_logs (uid, email, last_login, role) VALUES ($1,$2,$3, $4) ON CONFLICT (email) DO UPDATE SET last_login = EXCLUDED.last_login";
 const logCreateTodo = "UPDATE user_logs SET created_todos = $1 WHERE uid = $2";
 const logDeleteTodo = "UPDATE user_logs SET deleted_todos = $1 WHERE uid = $2";
